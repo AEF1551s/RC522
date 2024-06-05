912f93b6166767787754d742c61a7425d8b32c96
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-06-05 14:42:18 +0300

    Update CMake

--- toolchain.cmake
+++ toolchain.cmake
@@ -5,7 +5,7 @@ set(CMAKE_SYSTEM_PROCESSOR ARM)
 set(compiler_path /home/roboros/gcc-arm-none-eabi-10.3-2021.10)
 
 # Compiler paths
-set(CMAKE_CXX_COMPILER "${compiler_path}/bin/arm-none-eabi-g++")
+# set(CMAKE_CXX_COMPILER "${compiler_path}/bin/arm-none-eabi-g++")
 set(CMAKE_C_COMPILER "${compiler_path}/bin/arm-none-eabi-gcc")
 # Compiler flags
 set(compiler_flags
@@ -16,7 +16,7 @@ set(linker_flags
 )
 
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS} ${compiler_flags}")
-set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${compiler_flags}")
+# set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${compiler_flags}")
 set(CMAKE_EXE_LINKER_FLAGS "${linker_flags}")
 
 set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
