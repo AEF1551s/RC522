74eadfeb9bbfa6541b3db639c9cb6792024de144
Krisjanis Ivbulis <ivbulisk@gmail.com>
2024-06-06 12:14:22 +0300

    EXTI button press

--- src/main.c
+++ src/main.c
@@ -1,29 +1,57 @@
 #include <stm32f410rx.h>
 #include <stm32f4xx.h>
-#include <startup.c>
+// #include <startup.c>
 #include <stdbool.h>
+#include <gpio.h>
+#include <exti.h>
 
-//TODO: Add SPI, GPIO, TIMER headers.
-
-void exti_interrupt_init()
-{
-    // EXTI Control Register 3, Set IRQ for PA11;
-    SET_BIT(SYSCFG->EXTICR[3], SYSCFG_EXTICR3_EXTI11_PA << SYSCFG_EXTICR3_EXTI11_Pos);
-
-    // TODO: SPI interupts
-    // TODO: TIMER interupts
-}
-
-void gpio_init()
-{
-    // PA11 input, pulldown resistor
-}
+// TODO: Add SPI, GPIO, TIMER headers.
 
 int main()
 {
+
+    gpio_init();
+    exti_init();
+
     while (true)
     {
+        
+        if (READ_BIT(GPIOB->IDR, GPIO_IDR_IDR_11))
+        {
 
+            // ON LED
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS13);
+
+            // wait
+            for (volatile int i = 0; i < 1000000; i++)
+                ;
+            // OFF LED
+            SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR13);
+            // wait
+            for (volatile int i = 0; i < 1000000; i++)
+                ;
+        }
     }
+
     return 0;
+}
+
+// Interrupts
+void EXTI15_10_IRQHandler(void)
+{
+    // if interrupt happens on 11 pin
+    if (READ_BIT(EXTI->PR, EXTI_PR_PR11))
+    {
+        // Clear pending bit by writing 1 per RefManual. reset interupt request
+        SET_BIT(EXTI->PR, EXTI_PR_PR11);
+
+        // ON LED
+        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BS13);
+
+        // wait
+        for (volatile int i = 0; i < 1000000; i++)
+            ;
+        // OFF LED
+        SET_BIT(GPIOB->BSRR, GPIO_BSRR_BR13);
+    }
 }
